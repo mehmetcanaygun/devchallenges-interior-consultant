06f019d2ce18744083ffca0d00ab66ac6e34acf9
Can <mca199667@gmail.com>
2020-11-06 17:48:58 +0300

    Prevent scrolling when sidebar toggled

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ const menuBtn = document.getElementById("menu-btn");
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("toggled");
   navbar.classList.toggle("toggled");
+
+  if(navbar.classList.contains("toggled")) {
+    document.body.style.overflowY = "hidden";
+  } else {
+    document.body.style.overflowY = "visible";
+  }
 });
